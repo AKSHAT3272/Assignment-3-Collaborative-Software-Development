738ff6ed6f96ce6053f9da9d3b05af9a514484d8
Michael Pascale <michalpascal3557@adelphi.mail.edu>
2019-10-15 10:27:58 -0400

    Changed some references in the file. Also build the database. This script will reset the db if we make a mistake adding or removing data

--- config/createdb.sql
+++ config/createdb.sql
@@ -8,21 +8,20 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema michaelpascale
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema michaelpascale
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `michaelpascale` DEFAULT CHARACTER SET utf8 ;
+USE `michaelpascale` ;
+-- -----------------------------------------------------
+-- Table `michaelpascale`.`Challenge`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `michaelpascale`.`Challenge` ;
 
--- -----------------------------------------------------
--- Table `mydb`.`Challenge`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Challenge` ;
-
-CREATE TABLE IF NOT EXISTS `mydb`.`Challenge` (
+CREATE TABLE IF NOT EXISTS `michaelpascale`.`Challenge` (
   `challengeID` INT NOT NULL AUTO_INCREMENT,
   `problem` LONGTEXT NOT NULL,
   `solution` LONGTEXT NOT NULL,
@@ -32,11 +31,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Login`
+-- Table `michaelpascale`.`Login`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`Login` ;
+DROP TABLE IF EXISTS `michaelpascale`.`Login` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`Login` (
+CREATE TABLE IF NOT EXISTS `michaelpascale`.`Login` (
   `username` VARCHAR(16) NOT NULL,
   `password` VARCHAR(16) NOT NULL,
   PRIMARY KEY (`username`),
@@ -46,11 +45,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`User`
+-- Table `michaelpascale`.`User`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`User` ;
+DROP TABLE IF EXISTS `michaelpascale`.`User` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`User` (
+CREATE TABLE IF NOT EXISTS `michaelpascale`.`User` (
   `username` VARCHAR(16) NOT NULL,
   `isParticipant` TINYINT NOT NULL DEFAULT 0,
   `isAdmin` TINYINT NOT NULL DEFAULT 0,
@@ -59,18 +58,18 @@ CREATE TABLE IF NOT EXISTS `mydb`.`User` (
   UNIQUE INDEX `Login_username_UNIQUE` (`username` ASC) VISIBLE,
   CONSTRAINT `fk_User_Login`
     FOREIGN KEY (`username`)
-    REFERENCES `mydb`.`Login` (`username`)
+    REFERENCES `michaelpascale`.`Login` (`username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`ParticipantChallenge`
+-- Table `michaelpascale`.`ParticipantChallenge`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`ParticipantChallenge` ;
+DROP TABLE IF EXISTS `michaelpascale`.`ParticipantChallenge` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`ParticipantChallenge` (
+CREATE TABLE IF NOT EXISTS `michaelpascale`.`ParticipantChallenge` (
   `username` VARCHAR(16) NOT NULL,
   `challengeID` INT NOT NULL,
   `attempt` LONGTEXT NULL,
@@ -79,35 +78,35 @@ CREATE TABLE IF NOT EXISTS `mydb`.`ParticipantChallenge` (
   INDEX `fk_ParticipantChallenge_Challenge1_idx` (`challengeID` ASC) VISIBLE,
   CONSTRAINT `fk_ParticipantChallenge_User1`
     FOREIGN KEY (`username`)
-    REFERENCES `mydb`.`User` (`username`)
+    REFERENCES `michaelpascale`.`User` (`username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_ParticipantChallenge_Challenge1`
     FOREIGN KEY (`challengeID`)
-    REFERENCES `mydb`.`Challenge` (`challengeID`)
+    REFERENCES `michaelpascale`.`Challenge` (`challengeID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`OrganizerChallenge`
+-- Table `michaelpascale`.`OrganizerChallenge`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`OrganizerChallenge` ;
+DROP TABLE IF EXISTS `michaelpascale`.`OrganizerChallenge` ;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`OrganizerChallenge` (
+CREATE TABLE IF NOT EXISTS `michaelpascale`.`OrganizerChallenge` (
   `username` VARCHAR(16) NOT NULL,
   `challengeID` INT NOT NULL,
   PRIMARY KEY (`username`, `challengeID`),
   INDEX `fk_OrganizerChallenge_Challenge1_idx` (`challengeID` ASC) VISIBLE,
   CONSTRAINT `fk_OrganizerChallenge_User1`
     FOREIGN KEY (`username`)
-    REFERENCES `mydb`.`User` (`username`)
+    REFERENCES `michaelpascale`.`User` (`username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_OrganizerChallenge_Challenge1`
     FOREIGN KEY (`challengeID`)
-    REFERENCES `mydb`.`Challenge` (`challengeID`)
+    REFERENCES `michaelpascale`.`Challenge` (`challengeID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;

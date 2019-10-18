26e2f8ec569c69645cf9be33fc3ac03f0bef8409
Michael Pascale <michalpascal3557@adelphi.mail.edu>
2019-10-18 18:28:03 -0400

    Added a query which gets the privileges for the user

--- src/session.php
+++ src/session.php
@@ -8,6 +8,10 @@
    $user_check = $_SESSION['login_user'];
 
    $ses_sql = mysqli_query($db,"select username from Login where username = '$user_check' ");
+   
+   $roles_query = "select isparticipant, isadmin, isorganizer from User where username = '$user_check'";
+   $roles_sql = mysqli_query($db, $roles_query);
+   $roles_values = mysqli_fetch_array($roles_sql,MYSQLI_ASSOC);
 
    $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
 
